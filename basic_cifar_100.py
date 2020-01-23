294a921682b02bd3e14d8197554e999efbc75fd5
Ian Conway <iantheconway@gmail.com>
2020-01-22 21:24:09 -0800

    added transfer learning

--- basic_cifar_100.py
+++ basic_cifar_100.py
@@ -4,15 +4,18 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
 from class_names import cifar_100_classes
+from tensorflow.keras.utils import to_categorical
 
-
-(train_images_coarse, train_labels_coarse), (test_images_coarse, test_labels_coarse) = datasets.cifar100.load_data(label_mode='coarse')
+(train_images, train_labels), (test_images, test_labels) = datasets.cifar100.load_data()
+(train_images_coarse, train_labels_coarse), (test_images_coarse, test_labels_coarse) = datasets.cifar100.load_data(
+    label_mode='coarse')
 
 # Normalize pixel values to be between 0 and 1
 train_images, test_images = train_images_coarse / 255.0, test_images_coarse / 255.0
 
 class_names = [coarse_label for coarse_label in cifar_100_classes]
 
+# Sanity Check: plot examples of each coarse class
 plt.figure(figsize=(10, 10))
 for i in range(25):
     plt.subplot(5, 5, i + 1)
@@ -24,6 +27,8 @@ for i in range(25):
     # which is why you need the extra index
     plt.xlabel(class_names[train_labels_coarse[i][0]])
 plt.show()
+
+# Define model for training on the coarse classes
 
 model = models.Sequential()
 model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
@@ -47,13 +52,44 @@ model.compile(optimizer='adam',
 history = model.fit(train_images_coarse, train_labels_coarse, epochs=10,
                     validation_data=(test_images_coarse, test_labels_coarse))
 
+print("training on fine classes")
+
+# freeze layers from coarse class model
+
+for layer in model.layers:
+    layer.trainable = False
+
+print("summary before removing layer")
+model.summary()
+
+# Create a new model for training on the fine classes
+model_2 = models.Sequential()
+
+for layer in model.layers[:-1]:
+    model_2.add(layer)
+
+print("summary before adding new layer")
+
+# Add a trainable final layer
+model_2.add(layers.Dense(100, activation='softmax'))
+
+print("summary after adding new layer")
+model_2.build(input_shape=model.input_shape)
+model_2.summary()
+model_2.compile(optimizer='adam',
+                loss='categorical_crossentropy',
+                metrics=['accuracy'])
+
+history = model_2.fit(train_images, to_categorical(train_labels), epochs=10,
+                      validation_data=(test_images, to_categorical(test_labels)))
+
 plt.plot(history.history['accuracy'], label='accuracy')
-plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+plt.plot(history.history['val_accuracy'], label='val_accuracy')
 plt.xlabel('Epoch')
 plt.ylabel('Accuracy')
 plt.ylim([0.5, 1])
 plt.legend(loc='lower right')
-
-test_loss, test_acc = model.evaluate(test_images_coarse,  test_labels_coarse, verbose=2)
+test_loss, test_acc = model.evaluate(test_images_coarse, test_labels_coarse, verbose=2)
 
 print(test_acc)
+plt.show()
