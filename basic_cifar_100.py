ac4a00c444c12130756d22f5b95f1ad96bd47c7c
Ian Conway <iantheconway@gmail.com>
2020-01-23 14:19:38 -0800

    filtered classes

--- basic_cifar_100.py
+++ basic_cifar_100.py
@@ -3,21 +3,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
-from class_names import cifar_100_classes
+from class_names import cifar_100_coarse_classes
 from tensorflow.keras.utils import to_categorical
+
+
+# n_classes must be <= 20. In 1909.11572 they use only 3 coarse classes
+n_classes = 3
+learning_rate = 0.0129
+batch_size = 128
 
 (train_images, train_labels), (test_images, test_labels) = datasets.cifar100.load_data()
 (train_images_coarse, train_labels_coarse), (test_images_coarse, test_labels_coarse) = datasets.cifar100.load_data(
     label_mode='coarse')
 
-# Normalize pixel values to be between 0 and 1
-# train_images_coarse, test_images_coarse = train_images_coarse / 255.0, test_images_coarse / 255.0
-# train_images, test_images = train_images / 255.0, test_images / 255.0
+# Filter based on class labels
 
-learning_rate = 0.0129
-batch_size = 128
 
-class_names = [coarse_label for coarse_label in cifar_100_classes]
+train_filter = np.where(train_labels_coarse < 3)[0]
+test_filter = np.where(test_labels_coarse < 3)[0]
+
+train_images, train_labels = train_images[train_filter], train_labels[train_filter]
+test_images, test_labels = test_images[test_filter], test_labels[test_filter]
+
+train_images_coarse, train_labels_coarse = train_images_coarse[train_filter], train_labels_coarse[train_filter]
+test_images_coarse, test_labels_coarse = test_images_coarse[test_filter], test_labels_coarse[test_filter]
+
+class_names = [coarse_label for coarse_label in cifar_100_coarse_classes[:n_classes]]
 
 # Sanity Check: plot examples of each coarse class
 plt.figure(figsize=(10, 10))
@@ -27,40 +38,43 @@ for i in range(25):
     plt.yticks([])
     plt.grid(False)
     plt.imshow(train_images[i], cmap=plt.cm.binary)
-    # The CIFAR labels happen to be arrays,
-    # which is why you need the extra index
     plt.xlabel(class_names[train_labels_coarse[i][0]])
 plt.show()
+# Normalize pixel values to be between 0 and 1
+train_images_coarse, test_images_coarse = train_images_coarse / 255.0, test_images_coarse / 255.0
+train_images, test_images = train_images / 255.0, test_images / 255.0
 
 # Define model for training on the coarse classes
 # Defining total DOF and penultimate layer size:
 n_DOF = 1024
 n_penul = 512
-# Final layer size determined by number of classes:
-n_classes = 20
+
 
 model = models.Sequential()
 model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=train_images[0].shape))
 model.add(layers.Conv2D(32, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Dropout(0.25))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.Conv2D(32, (3, 3), activation='relu'))
+model.add(layers.Conv2D(32, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Dropout(0.25))
 model.add(layers.Flatten())
 # model.add(layers.Dense(np.floor_divide(n_DOF, n_penul), activation='relu'))
 model.add(layers.Dense(n_penul, activation='relu'))
+model.add(layers.Dropout(0.5))
 model.add(layers.Dense(n_classes, activation='softmax'))
 
 model.summary()
 
-sgd = tf.keras.optimizers.SGD(learning_rate=learning_rate, clipnorm=1., momentum=.9)
+sgd = tf.keras.optimizers.SGD(learning_rate=learning_rate, momentum=.9)
+adam = tf.keras.optimizers.Adam(learning_rate=learning_rate)
 
 model.compile(
-    optimizer=sgd,
+    # optimizer=sgd,
+    optimizer=adam,
     loss='sparse_categorical_crossentropy',
-    metrics=['accuracy'])
+    metrics=['accuracy', 'categorical_accuracy'])
 
 history = model.fit(train_images_coarse, train_labels_coarse, epochs=100,
                     validation_data=(test_images_coarse, test_labels_coarse),
@@ -92,7 +106,7 @@ model_2.build(input_shape=model.input_shape)
 model_2.summary()
 model_2.compile(optimizer='adam',
                 loss='categorical_crossentropy',
-                metrics=['accuracy'])
+                metrics=['accuracy', 'categorical_accuracy'])
 
 history = model_2.fit(train_images, to_categorical(train_labels), epochs=100
                       ,
